68be8f1986a5b1dc46fba2fcc85ea625cfb4094c
root <root@localhost.localdomain>
2019-06-23 21:35:14 +0530

    implementing m2m_changed signal to calculate cart total.

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.db import models
 
+from django.db.models.signals import (
+                                    pre_save,
+                                    post_save,
+                                    m2m_changed)
+
 # Create your models here.
 User = settings.AUTH_USER_MODEL
 from products.models import Product
@@ -34,6 +39,7 @@ class CartManager(models.Manager):
 class Cart(models.Model):
     user        = models.ForeignKey(User,blank=True, null=True,on_delete=True)
     products    = models.ManyToManyField(Product,blank=True)
+    subtotal    = models.DecimalField(decimal_places=2,default=0.00,max_digits=100)
     total       = models.DecimalField(decimal_places=2,default=0.00,max_digits=100)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
@@ -42,3 +48,22 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+def m2m_changed_cart_receiver(sender, instance, action,*args, **kwargs):
+    print(action)
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear ':
+        products = instance.products.all()
+        total = 0
+        for x in products:
+            total += x.price
+        if instance.total!=total:
+            instance.subtotal = total
+            instance.save()
+
+m2m_changed.connect(m2m_changed_cart_receiver , sender=Cart.products.through)
+
+
+def pre_save_cart_receiver(sender, instance,*args, **kwargs):
+    instance.total = instance.subtotal
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
--- src/carts/views.py
+++ src/carts/views.py
@@ -5,8 +5,7 @@ from .models import Cart
 # Create your views here.
 
 def cart_home(request):
-    cart_obj = Cart.objects.new_or_get(request)
-    
+    cart_obj,new_obj = Cart.objects.new_or_get(request)
     return render(request, 'carts/home.html', context=None)
     
     
